243f8aeb1d5d622b998a4b83d1f0c7b0c183229a
Vance Harris <53877381+harrisv25@users.noreply.github.com>
2021-07-07 09:23:56 -0600

    Update RasterAdd_Raster_Demensions_in_Array.py

--- RasterAdd_Raster_Demensions_in_Array.py
+++ RasterAdd_Raster_Demensions_in_Array.py
@@ -1,4 +1,6 @@
+#create empty array of the raster shape, followed by the number of rasters
 raster = np.empty((1974, 1894, 97))
 
+#wraite the raster to the array
 for k in range(len(rasters)):
     raster[:,:,k] = rasterio.open(rasters[k], 'r').read()[0]
